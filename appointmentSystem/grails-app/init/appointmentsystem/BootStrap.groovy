7b67a7f99ded65a1517bbd55b2991ed56454d038
student <student@b7002006.localdomain>
2019-03-19 16:31:50 +0000

    created new branch

--- appointmentSystem/grails-app/init/appointmentsystem/BootStrap.groovy
+++ appointmentSystem/grails-app/init/appointmentsystem/BootStrap.groovy
@@ -3,6 +3,27 @@ package appointmentsystem
 class BootStrap {
 
     def init = { servletContext ->
+
+//define objects
+
+//Appointment
+
+//doctor
+
+//nurse
+
+//patient
+
+
+
+
+
+
+
+
+
+
+
     }
     def destroy = {
     }
